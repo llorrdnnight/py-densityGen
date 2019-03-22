9eea47bea8132143d0b6f3445e6b867e198e25f3
Unknown <levi.nauwelaerts@gmail.com>
2019-03-22 13:35:10 +0100

    it kinda works

--- NoiseGen.py
+++ NoiseGen.py
@@ -4,46 +4,47 @@ import time
 import numpy as np
 
 field = []
+global rows, colums
 rows = 0
 colums = 0
-#emptyLine = ["-"] * 10
 
 #Create the noisefield
 def generateField(rows_p, colums_p):
-	rows = rows_p
-	colums = colums_p
-	emptyRow = ["-"] * colums
-	for x in range(rows-1):
-		field.append(emptyRow)
+    global rows, colums
+    rows = rows_p
+    colums = colums_p
+    emptyRow = ["-"] * colums
+    for x in range(rows-1):
+        field.append(emptyRow)
 
 def printField():
     print(np.matrix(field))
 
 def testField():
-	x = 10
-	x ++
-	y = 15
-	generateField(x, y)
-	init_world_anchors()
-	printField()
-	time.sleep(20)
+    x = 10
+    x = x+1
+    y = 15
+    generateField(x, y)
+    init_world_anchors()
+    printField()
+    time.sleep(20)
 
 #Init first achors of world
 '''
-	We run over the field, for each position we make a check, if the check applies 
+    We run over the field, for each position we make a check, if the check applies
 '''
 def init_world_anchors():
-	for i in range(rows):
-		for j in range(colums):
-			chance = random.randomint(1,100)
-			if chance < 6:
-				#add weigth
-				field[i][j] = 100
-			else:
-				field[i][j] = "+"
-
-
-
+    global rows, colums
+    for i in range(rows-1):
+        for j in range(colums):
+            chance = random.randint(1,100)
+            time.sleep(0.01)
+            if chance < 11:
+                field[i][j] = 100
+            else:
+                field[i][j] = "+"
+            print(chance)
+            print(field[i][j])
 
 #The actual runtime
 testField()
